8d0b760d5e5efe81009e4c412e3f31e7e0d1420e
罗杰 <13369189710@163.com>
2021-02-01 18:42:08 +0800

    关于变量与可变性的 test

---
+++ ch3/variables/src/main.rs
@@ -0,0 +1,12 @@
+const MAX_POINTS: u32 = 100_000;
+fn main() {
+	println!("The value of MAX_POINTS is: {}", MAX_POINTS);
+	let mut x = 5;
+	println!("The value of x is: {}", x);
+	x = 6;
+	println!("The value of x is: {}", x);
+	let spaces = "    ";
+	println!("The value of spaces is: {}", spaces);
+	let spaces = spaces.len();
+	println!("The value of spaces is: {}", spaces);
+}
