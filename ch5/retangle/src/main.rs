78b4b81bf4b82300a7c67f0c6aa2037d2beab634
luojie <13369189710@163.com>
2021-02-06 11:58:24 +0800

    方法相关代码

--- ch5/retangle/src/main.rs
+++ ch5/retangle/src/main.rs
@@ -4,15 +4,47 @@ struct Rectangle {
 	height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        return self.width * self.height;
+    }
+
+    fn can_hold(&self, dest: &Rectangle) -> bool {
+        self.area() > dest.area()
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+}
+
 fn main() {
 	let rec = Rectangle {
 		width: 30,
 		height: 50,
 	};
 
-	println!("area: {}", area(&rec));
-	println!("Rectangle: {:?}", rec);
-	println!("Rectangle: {:#?}", rec);
+    let rec2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+
+    let rec3 = Rectangle {
+        width: 40,
+        height: 60,
+    };
+	println!("area(&rec): {}", area(&rec));
+	println!("rec.area(): {}", rec.area());
+	println!("rec: {:?}", rec);
+	println!("rec2: {:?}", rec2);
+    println!("rec.can_hold(rec2): {}", rec.can_hold(&rec2));
+    println!("rec.can_hold(rec3): {}", rec.can_hold(&rec3));
+	println!("rec: {:#?}", rec);
+    println!("Rectangle::square(3): {:#?}", Rectangle::square(3));
+
 }
 
 fn area(r: &Rectangle) -> u32 {
