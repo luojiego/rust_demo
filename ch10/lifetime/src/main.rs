53b7b817df8e672d982b182d09dc189dd5347328
罗杰 <13369189710@163.com>
2021-02-20 21:55:15 +0800

    完善 lifetime 相关内容

--- ch10/lifetime/src/main.rs
+++ ch10/lifetime/src/main.rs
@@ -1,4 +1,6 @@
 fn main() {
+    let static_lifetime_variable: &'static str = "I have a static lifetime.";
+
     let str1 = String::from("abcd");
     let str2 = "def";
     let result = longest(str1.as_str(), str2);
@@ -28,7 +30,52 @@ fn main() {
     println!("the longest string is: {}", result3);
 
     // String::from("abc") 和 ”abc“ 的区别需要研究一下
+
+    let novel = String::from("Call me. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
+    println!("i: {:#?}", i);
+    println!("i.announce_and_return_part(\"HaHa...\"): {}", i.announce_and_return_part("Haha"));
+
+    println!("first_word(\"luo jie\"): {}", first_word("luo jie"));
+
+    println!("static lifetime: {}", static_lifetime_variable);
 }
+
+// lifetime elison 不用声明 lifetime
+fn first_word(s: &str) -> &str {
+    // static 但是为什么不能在此打印 static_lifetime_variable
+    // println!("static lifetime: {}", static_lifetime_variable);
+
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+    &s[..]
+}
+
+#[derive(Debug)]
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+impl<'a> ImportantExcerpt<'a> {
+    fn level(&self) -> i32 {
+        3
+    }
+}
+
+impl<'a> ImportantExcerpt<'a> {
+    fn announce_and_return_part(&self, announcement: &str) -> &str {
+        println!("Attention please: {}", announcement);
+        self.part
+    }
+}
+
 // 错误示范
 // fn longest(x: &str, y: &str) -> &str {
 // //            ----     ----     ^expected named lifetime parameter
