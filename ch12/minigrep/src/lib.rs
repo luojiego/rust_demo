49d98302dacf7e598af03a3c1615b0bdb0d69893
罗杰 <13369189710@163.com>
2021-02-22 17:13:41 +0800

    minigrep 代码完成

---
+++ ch12/minigrep/src/lib.rs
@@ -0,0 +1,106 @@
+use std::error::Error;
+use std::fs;
+use std::env;
+// 优化7
+// 从 main.rs 把 struct 的定义和 run 函数迁移出来
+
+// 优化 2
+// 优化 3
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+    pub case_sensitive: bool,
+}
+
+// 优化 3
+impl Config {
+    // 优化 4 错误参数
+    // fn new(args: &[String]) -> Config {
+    //     if args.len() < 3 {
+    //         panic!("not enough arguments");
+    //     }
+    //     let query = args[1].clone(); 
+    //     let filename = args[2].clone(); 
+    //     Config {query, filename}
+    // }
+
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        // 优化 4 错误参数
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+        let query = args[1].clone(); 
+        let filename = args[2].clone(); 
+        Ok(Config {query, filename, case_sensitive})
+    }
+}
+
+// 优化 6
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+    // println!("With text:\n{}", contents);
+
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{}", line);
+    }
+    Ok(())
+} 
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+// 优化 8
+// 增加大小写不敏感
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    let query = query.to_lowercase();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+        assert_eq!(vec!["Rust:", "Trust me."], search_case_insensitive(query, contents));
+    }
+}
---
+++ ch12/minigrep/src/main.rs
@@ -0,0 +1,71 @@
+use std::process;
+use std::env;
+use minigrep::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    // 优化
+    // println!("{:#?}", args);
+    // let query = &args[1];
+    // let filename = &args[2];
+
+    // 优化1
+    // let (query, filename) = parse_config(&args);
+    // println!("Searching for {}", query);
+    // println!("In file {}", filename);
+
+    // 优化 2
+    // let config = parse_config(&args);
+
+    // 优化 3
+    // let config = Config::new(&args);
+
+    // 优化 4
+    let config = Config::new(&args).unwrap_or_else(|err|{
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    // let contents = fs::read_to_string(config.filename)
+    //     .expect("Something went wrong reading a file");
+
+    // println!("With text: \n{}", contents);
+
+    // 优化 5
+    // run(config);
+
+    // 优化 6
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
+}
+
+// 优化1
+// fn parse_config(args: &[String]) -> (&str, &str) {
+//     let query = &args[1];
+//     let filename = &args[2];
+// 
+//     (query, filename)
+// }
+
+
+
+// fn run(config: Config) {
+//     let contents = fs::read_to_string(config.filename)
+//         .expect("Something went wrong reading the file");
+//     println!("With text:\n{}", contents);
+// }
+
+
+
+// 优化 2
+// fn parse_config(args: &[String]) -> Config {
+//    let query = args[1].clone(); 
+//    let filename = args[2].clone(); 
+// 
+//    Config {query, filename}
+// }
