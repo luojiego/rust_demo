e61af55dc0f01bc9e281581ac09d6b426ba12d1f
luojie <13369189710@163.com>
2021-02-28 16:04:25 +0800

    macros 研究，未完成

---
+++ ch19/hello_macro_derive/src/lib.rs
@@ -0,0 +1,12 @@
+extern crate proc_macro;
+
+use proc_macro::TokenStream;
+use quote::quote;
+use syn;
+
+#[proc_macro_derive(HelloMacro)]
+pub fn hello_macro_derive(input: TokenStream) -> TokenStream {
+    let ast = syn::parse(input).unwrap();
+
+    impl_hello_macro(&ast)
+}
