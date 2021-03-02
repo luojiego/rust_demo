e8649f86bf5a8e1f331643eea21552daad902a9f
罗杰 <13369189710@163.com>
2021-03-02 14:55:54 +0800

    暂时无法编译通过

--- protobuf/src/main.rs
+++ protobuf/src/main.rs
@@ -1,5 +1,13 @@
+extern crate protobuf;
+// use protobuf_demo::Person;
 // cargo install protobuf-codegen 安装相关软件包 protoc-gen-rust
+pub mod hello {
+    include!(concat!(env!("OUT_DIR"), "/hello.rs"));
+}
+
 fn main() {
     println!("Hello, world!");
+    // let p = crate::Person::new();
+    // println!("p: {}", p);
 }
 
