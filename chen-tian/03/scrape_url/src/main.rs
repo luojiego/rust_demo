d1636b8feb34e2aca4aac33358bac6c6e656f8c3
罗杰 <13369189710@163.com>
2021-09-01 18:40:22 +0800

    从命令行获取参数

--- chen-tian/03/scrape_url/src/main.rs
+++ chen-tian/03/scrape_url/src/main.rs
@@ -1,10 +1,24 @@
 use std::fs;
 
 fn main() {
+    // let mut i : u8 = 0;
+    // for arg in std::env::args() {
+    //     println!("index: {} args: {}", i, arg);
+    //     i+=1;
+    // }
+
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() <= 2 {
+        println!("Usage: {:?} [url] [result markdown file path]", args[0]);
+        return
+    }
+
     // let url = "https://www.rust-lang.org/";
     // let output = "rust.md";
-    let url = "https://github.com/lesismal/arpc";
-    let output = "arpc.md";
+    // let url = "https://github.com/lesismal/arpc";
+    // let output = "arpc.md";
+    let url = &args[1];
+    let output = &args[2]; 
 
     println!("Fetching url: {}", url);
     let body = reqwest::blocking::get(url).unwrap().text().unwrap();
