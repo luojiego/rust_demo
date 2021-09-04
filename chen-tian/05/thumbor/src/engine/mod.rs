afcf826dfa20ef0cbed145eee03c3acc36759018
罗杰 <13369189710@163.com>
2021-09-04 18:03:49 +0800

    图片服务器功能完成，好难啊

---
+++ chen-tian/05/thumbor/src/engine/mod.rs
@@ -0,0 +1,20 @@
+use crate::pb::Spec;
+use image::ImageOutputFormat;
+
+// Engin trait: 未来可添加更多的 engine，主流程只需要替换 engine
+mod photon;
+pub use photon::Photon;
+
+pub trait Engine {
+    // 对 engine 按照 specs 进行一系列的处理
+    fn apply(&mut self, specs: &[Spec]);
+
+    // 对 engine 中生成目标图片，注意这里用的是 Self，而非 self 的引用
+    fn generate(self, format: ImageOutputFormat) -> Vec<u8>;
+}
+
+// SpecTransform: 未来如果添加更多的 spec，只需要实现即可
+pub trait SpecTransform<T> {
+    // 对图片使用 op 做 transform
+    fn transform(&mut self, op: T);
+}
