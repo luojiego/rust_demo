9f161e0d5162f3328763c1eceb9cebdec5f17c09
罗杰 <13369189710@163.com>
2021-10-18 22:54:20 +0800

    修复在 Windows 下面的编译问题

--- chen-tian/21/kv/build.rs
+++ chen-tian/21/kv/build.rs
@@ -2,6 +2,7 @@
 fn main() {
     prost_build::Config::new()
         .out_dir("src/pb")
+        .file_descriptor_set_path("target/tmp")
         .compile_protos(&["abi.proto"], &["."])
         .unwrap();
     // prost_build::compile_protos(&["abi.proto", &["."])?;
--- chen-tian/21/kv/src/pb/abi.rs
+++
@@ -1,5 +0,0 @@
-#[derive(Clone, PartialEq, ::prost::Message)]
-pub struct Hello {
-    #[prost(uint32, tag="1")]
-    pub age: u32,
-}
