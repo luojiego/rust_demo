70bc705c3b15d0c57acd2c7c879a3b8e12910eb9
罗杰 <13369189710@163.com>
2021-10-24 11:13:05 +0800

    mod.rs 文件提交

---
+++ chen-tian/21/kv/src/pb/mod.rs
@@ -0,0 +1,44 @@
+
+pub mod abi;
+
+use abi::{command_request::RequestData, *};
+
+impl CommandRequest {
+    /// 创建 HSET 命令
+    pub fn new_hset(table: impl Into<String>, key: impl Into<String>, value: Value) -> Self {
+        Self {
+            request_data: Some(RequestData::Hset(Hset {
+                table: table.into(),
+                pair: Some(Kvpair::new(key, value)),
+            })),
+        }
+    }
+}
+
+impl Kvpair {
+    /// 创建一个新的 kv pair
+    pub fn new(key: impl Into<String>, value: Value) -> Self {
+        Self {
+            key: key.into(),
+            value: Some(value),
+        }
+    }
+}
+
+/// 从 String 转换成 Value
+impl From<String> for Value {
+    fn from(s: String) -> Self {
+        Self {
+            value: Some(value::Value::String(s)),
+        }
+    }
+}
+
+/// 从 &str 转换成 Value
+impl From<&str> for Value {
+    fn from(s: &str) -> Self {
+        Self {
+            value: Some(value::Value::String(s.into())),
+        }
+    }
+}
