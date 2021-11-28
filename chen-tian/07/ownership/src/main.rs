0191704c0a20be1f3135ad30857adcc4f4663370
罗杰 <13369189710@163.com>
2021-11-28 14:09:14 +0800

    完善所有权代码

--- chen-tian/07/ownership/src/main.rs
+++ chen-tian/07/ownership/src/main.rs
@@ -20,19 +20,33 @@ fn types_impl_copy_trait() {
     is_copy::<(&str,&str)>();
 }
 
+// 以下类型均没有实现 Copy
 fn types_not_impl_copy_trait() {
-    is_copy::<str>();
-    is_copy::<[u8]>();
-    is_copy::<Vec[u8]>();
-    is_copy::<String>();
-
-    is_copy::<&mut String>();
-
-    is_copy::<[Vec<u8>;4]>();
-    is_copy::<(String, u32)>();
+    // is_copy::<str>();
+    // is_copy::<[u8]>();
+    // is_copy::<Vec[u8]>();
+    // is_copy::<String>();
+    // is_copy::<&mut String>();
+    // is_copy::<[Vec<u8>;4]>();
+    // is_copy::<(String, u32)>();
 }
 
 fn main() {
     types_impl_copy_trait();
     types_not_impl_copy_trait();
+
+    let data = vec![10, 42, 9, 8];
+    let v = 42;
+    if let Some(pos) = find_pos(data, v) {
+        println!("Found {} at {}", v, pos);
+    }
+}
+
+fn find_pos(data: Vec<u32>, v:u32) -> Option<usize> {
+    for (pos, item) in data.iter().enumerate() {
+        if *item == v {
+            return Some(pos);
+        }
+    }
+    None
 }
